3536f9dc0de223409e7d6e9aa13ca5d9609e33a9
GabrielAsis <asisgab24@gmail.com>
2023-05-22 10:58:45 +0400

    carousel

--- script.js
+++ script.js
@@ -31,4 +31,24 @@ const fade = new IntersectionObserver((entries) => {
 const fadeElements = document.querySelectorAll('.fade-animation');
 fadeElements.forEach((el) => fade.observe(el));
 
+//carousel
+const carousel=document.querySelector('.carousel');
+let isDragStart= false;
+
+const dragStart= () =>{
+    isDragStart= true;
+}
+
+const dragging= (e) => {
+    if(!isDragStart) return;
+    e.preventDefault();
+    carousel.scrollLeft = e.pageX;
+}
+
+const dragStop= () =>{
+    isDragStart= false;
+}
+carousel.addEventListener("mousedown", dragStart)
+carousel.addEventListener("mousemove", dragging)
+carousel.addEventListener("mouseup", dragStop)
 
